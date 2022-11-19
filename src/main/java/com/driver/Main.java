9149ede0be7b2db1016beefdd598a1d47453cc68
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-11-19 16:16:53 +0530

    classes inside main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,17 @@
 package com.driver;
 
-class A{
+public class Main {
+   class A{
     public String meth(){
         return "Invoking method from class A";
     }
-}
-class B extends A {
+   }
+    
+  class B extends A {
     @Override public String meth(){
         return "Method is overridden in Extendend class B";
-    }
-}
-public class Main {
+     }
+   }
     public static void main(String[] args) {
         B instance = new B();
         String result = instance.meth();
